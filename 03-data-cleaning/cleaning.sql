cd148d888bfc27866c773b0e60ef13eb3323a2b2
Dulcie Sangin <dsangin123@gmail.com>
2026-04-24 20:46:45 +1000

    Update cleaning.sql

--- 03-data-cleaning/cleaning.sql
+++ 03-data-cleaning/cleaning.sql
@@ -1,1 +1,8 @@
---test here
+--Remove duplicates. (concept example)
+REMOVE DISTINCT Name FROM Customers;
+
+--Handle NULL values
+SELECT NAME ISNULL(Phone, 'No Phone') AS Phone FROM Customers;
+
+--Standardize text
+SELECT UPPER(Name) FROM Customers;
