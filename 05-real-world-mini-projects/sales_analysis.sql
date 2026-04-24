498dfbf3b9c810ca5a7d40e606c28250ee3b0dce
Dulcie Sangin <dsangin123@gmail.com>
2026-04-25 08:27:51 +1000

    Update sales_analysis.sql

--- 05-real-world-mini-projects/sales_analysis.sql
+++ 05-real-world-mini-projects/sales_analysis.sql
@@ -1,1 +1,16 @@
+--Total sales per customer
+SELECT CustomerID, SUM(Amount) AS TotalSales
+FROM Sales
+GROUP BY CustomerID;
 
+--Top 5 customers
+SELECT TOP 5 CustomerID, SUM(Amount) AS TotalSales
+FROM Sales
+GROUP BY CustomerID
+ORDER BY TotalSales DESC;
+
+--Monthly sales trend
+SELECT MONTH(SaleDate) AS Month, SUM(Amount) AS Total
+FROM Sales
+GROUP BY MONTH(SaleDate)
+ORDER BY Month;
