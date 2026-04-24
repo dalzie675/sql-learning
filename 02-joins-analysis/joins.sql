626c16ab6bda9c296e8313eae72b9b7360586a99
Dulcie Sangin <dsangin123@gmail.com>
2026-04-24 16:56:03 +1000

    Update joins.sql

--- 02-joins-analysis/joins.sql
+++ 02-joins-analysis/joins.sql
@@ -1,1 +1,11 @@
+--Inner Join example
+SELECT e.Name, d.DepartmentName
+FROM Employees e
+INNER JOIN Departments d
+ON e.DepartmentID = d.ID;
 
+--Left Join example
+SELECT e.Name, d.DepartmentName
+FROM Empployees e
+LEFT JOIN Departments d
+ON e.DepartmentID = d.ID;
