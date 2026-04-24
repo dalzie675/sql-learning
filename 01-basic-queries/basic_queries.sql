0a61b0ee1ce875f0b1af45d72b5d269c4a9cb292
Dulcie Sangin <dsangin123@gmail.com>
2026-04-24 16:48:36 +1000

    Update basic_queries.sql

--- 01-basic-queries/basic_queries.sql
+++ 01-basic-queries/basic_queries.sql
@@ -1,1 +1,10 @@
---test
+--Select all data
+SELECT * FROM Employees;
+
+--Filter records
+SELECT * FROM Employees
+WHERE Department = 'IT';
+
+--Sorting
+SELECT * FROM Employees
+ORDER BY Salary DESC;
